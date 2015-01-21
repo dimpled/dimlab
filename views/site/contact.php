05dd40805d29c2a93aebef58e35ab91d25d78f6a
LaZT <narongchaiwijitphan@Narongchai-MacBook-Pro.local>
2015-01-21 11:16:44 +0700

    เพิ่ม Dimpled.Me ในหน้า Contact

--- views/site/contact.php
+++ views/site/contact.php
@@ -34,6 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+        Dimpled.Me
     </p>
 
     <div class="row">
